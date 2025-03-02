4969d0dd72019d1043035fa0fbcf8dfb3ae6a3bf
stargazindreamr <49848622+stargazindreamr@users.noreply.github.com>
2025-03-02 18:23:14 -0500

    Update 02_prepare_openwrt_folder.sh

--- openwrt-24.10/steps/02_prepare_openwrt_folder.sh
+++ openwrt-24.10/steps/02_prepare_openwrt_folder.sh
@@ -10,9 +10,17 @@ cd "$ROOTDIR/build"
 
 cp -R openwrt-fresh-24.10 openwrt
 
-# freeze revision to 24.10.0
+# freeze revision to
+# 24.10.0
+REV_TAG="v24.10.0"
+REV_HASH="$(git rev-parse ${REV_TAG})"
+echo $REV_TAG
+echo $REV_HASH
+
+#REV_HASH="1fad1b4965dc6f4e5f4ba7b9605987f443a4c276"
 cd openwrt
-git reset --hard 1fad1b4965dc6f4e5f4ba7b9605987f443a4c276
+git reset --hard ${REV_HASH}
+REV_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 echo "Current OpenWRT commit"
 git log -1
